b554862483081c44a8a51ae2d4ef02e366455ccb
Eclipse <157326726+MoonWatcher19@users.noreply.github.com>
2025-12-09 14:17:37 -0700

    Update moves.ts

--- data/mods/stunfiskmeta/moves.ts
+++ data/mods/stunfiskmeta/moves.ts
@@ -3,5 +3,17 @@ export const Moves: import('../../../sim/dex-moves').ModdedMoveDataTable = {
 		inherit: true,
 		basePower: 60
 	},
-	
+	realbullet: {
+		// unfinished, will finalize when i have the time - Eclipse
+		accuracy: 100,
+		basepower: 0,
+		category: "Physical",
+		name: "Real Bullet",
+		pp: 1,
+		priority: 0,
+		flags: {},
+		ohko: true,
+		target: "normal",
+		type: "Steel",
+	},
 }
